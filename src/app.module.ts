e33c01e4fbbeb418aeefd0cf6a78664eae9091fe
palace22 <palazzolo1995@gmail.com>
2020-08-10 11:39:58 +0200

    added mongo credential

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module, HttpModule } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PollerModule } from './poller/poller.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TokenNetworkModule } from './token-network/token-network.module';
+import { environments } from './environments/environments';
+import { AppController } from 'src/app.controller';
 
 //const mongodb = 'mongodb://localhost:27017/raiden-map'
 const mongodb = 'mongodb://51.136.5.3:27017/raiden-map'
 
 @Module({
   imports: [
-    MongooseModule.forRoot(mongodb, {useFindAndModify: false, useCreateIndex: true}),
+    MongooseModule.forRoot(mongodb, {
+      user: environments.mongoUser, pass: environments.mongoPsw,
+      useFindAndModify: false
+    }),
     PollerModule,
     //TokenNetworkModule,
   ],
